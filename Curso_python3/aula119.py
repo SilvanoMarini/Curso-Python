b1149889189f4c2962337658a043a252a8ad865d
Silvano Junior <silvanomribeiro2002@gmail.com>
2025-08-05 18:55:46 -0300

    Retirada de comentarios

--- Curso_python3/aula119.py
+++ Curso_python3/aula119.py
@@ -1,48 +1,48 @@
 import os
 
-# tarefas = []
-# tarefas_desfeitas = []
+tarefas = []
+tarefas_desfeitas = []
 
-# while True:
-#     try:
-#         print('Escolha um comando ou digite uma tarefa para adicionar a lista')
-#         print('(Listar)  (Desfazer)  (Refazer) (Sair)')
+while True:
+    try:
+        print('Escolha um comando ou digite uma tarefa para adicionar a lista')
+        print('(Listar)  (Desfazer)  (Refazer) (Sair)')
 
-#         resposta = str(input('Digite: ')).lower()
-#         print()
+        resposta = str(input('Digite: ')).lower()
+        print()
 
-#         if resposta == 'listar':
-#             if len(tarefas) >= 1:
-#                 print(*tarefas, sep='\n')
-#                 print()
-#             else:
-#                 print('Sem Tarefas')
-#                 print()
+        if resposta == 'listar':
+            if len(tarefas) >= 1:
+                print(*tarefas, sep='\n')
+                print()
+            else:
+                print('Sem Tarefas')
+                print()
 
-#         elif resposta == 'desfazer':
-#             if len(tarefas) >= 1:
-#                 tarefas_desfeitas.append(tarefas.pop())
-#                 print()
-#             else:
-#                 print('Sem Tarefas para Desfazer')
-#                 print()
+        elif resposta == 'desfazer':
+            if len(tarefas) >= 1:
+                tarefas_desfeitas.append(tarefas.pop())
+                print()
+            else:
+                print('Sem Tarefas para Desfazer')
+                print()
 
-#         elif resposta == 'refazer':
-#             if len(tarefas_desfeitas) >= 1:
-#                 tarefas.append(tarefas_desfeitas.pop())
-#                 print()
-#             else:
-#                 print('Sem Tarefas Para Refazer')
-#                 print()
+        elif resposta == 'refazer':
+            if len(tarefas_desfeitas) >= 1:
+                tarefas.append(tarefas_desfeitas.pop())
+                print()
+            else:
+                print('Sem Tarefas Para Refazer')
+                print()
 
-#         elif resposta == 'clear':
-#             os.system('clear')
+        elif resposta == 'clear':
+            os.system('clear')
 
-#         elif resposta == 'sair':
-#             break
+        elif resposta == 'sair':
+            break
 
-#         else:
-#             tarefas.append(resposta)
-#     except Exception:
-#         print('ERRO! Tente novamente')
-#         continue
+        else:
+            tarefas.append(resposta)
+    except Exception:
+        print('ERRO! Tente novamente')
+        continue
