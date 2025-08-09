1f4731e6dea83f87c380647d3a5ee661879c4671
Silvano Junior <silvano.marini.dev@gmail.com>
2025-08-09 17:09:51 -0300

    Update

---
+++ Curso_python3/aula158/banco.py
@@ -0,0 +1,5 @@
+from abc import ABC
+
+class Banco(ABC):
+    def __init__(self):
+        super().__init__()
---
+++ Curso_python3/aula158/pessoas.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from contas_bancarias import ContaPoupanca
+
+class Pessoa(ABC):
+    def __init__(self, nome, idade, cpf):
+        super().__init__()
+        self._nome = nome
+        self._idede = idade
+        self._cpf = cpf
+
+
+    @property
+    def nome(self):
+        return self._nome
+
+
+    @property
+    def idade(self):
+        return self._idede
+
+
+
+class Cliente(Pessoa):
+    def __init__(self, nome, idade, cpf, renda):
+        super().__init__(nome, idade, cpf, renda)
+        self.renda = renda
+        self._contas = []
+
+    @property
+    def contas(self):
+        return self._contas
+
+    @contas.setter
+    def adicionar_conta(self, conta):
+        from contas_bancarias import Conta
+        if not isinstance(conta, Conta):
+           raise TypeError("Só é permitido adicionar objetos do tipo Conta.")
+        
+        if conta in self.contas:
+            raise ValueError("Essa conta já está cadastrada para este cliente.")
+
+        self._contas.append(conta)
+        conta.titular = self.nome
