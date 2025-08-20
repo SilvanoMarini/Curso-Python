60f381733584411f44260e86474ced0a51ae6a4d
Silvano Junior <silvano.marini.dev@gmail.com>
2025-08-19 22:41:32 -0300

    Update

--- Curso_python3/aula159.py
+++ Curso_python3/aula159.py
@@ -1,6 +1,15 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict, astuple
 
 @dataclass
 class Pessoa:
     nome: str
     idade: int
+
+
+p1 = Pessoa('Silvano', 22)
+print(p1)
+print(asdict(p1))
+print(asdict(p1).items())
+print(asdict(p1).keys())
+print(asdict(p1).values())
+print(astuple(p1))
