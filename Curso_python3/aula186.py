b340c49dd89262ed02c3e3b494f94e2d721fa3dd
Silvano Junior <silvano.marini.dev@gmail.com>
2025-08-23 15:33:04 -0300

    Update

--- Curso_python3/aula186.py
+++ Curso_python3/aula186.py
@@ -6,8 +6,8 @@ from zipfile import ZipFile
 
 CAMINHO_RAIZ = Path(__file__).parent
 CAMINHO_ZIP_DIR = CAMINHO_RAIZ / 'aula186_diretorio_zip'
-CAMINHO_COMPACTADO = CAMINHO_RAIZ / 'arquivo_compactado.zip'
-CAMINHO_DESCOMPACTADO =  CAMINHO_RAIZ / 'arquivo_descompactado'
+CAMINHO_COMPACTADO = CAMINHO_RAIZ / 'aula186_compactado.zip'
+CAMINHO_DESCOMPACTADO =  CAMINHO_RAIZ / 'aula186_descompactado'
 
 Path.mkdir(CAMINHO_ZIP_DIR, exist_ok=True)
 # print()
@@ -21,3 +21,18 @@ def criar_arquivos(qtd: int, Dir: Path):
 
 
 criar_arquivos(10, CAMINHO_ZIP_DIR)
+
+
+with ZipFile(CAMINHO_COMPACTADO, 'w') as zip:
+    for root, dirs, files in os.walk(CAMINHO_ZIP_DIR):
+        for file in files:
+            zip.write(os.path.join(root, file), file)
+
+
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    for arquivos in zip.namelist():
+        print(arquivos)
+
+
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    zip.extractall(CAMINHO_DESCOMPACTADO)
