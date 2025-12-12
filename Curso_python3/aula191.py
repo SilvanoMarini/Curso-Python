5c4dcc1d5197cbe0b115ac757aeaf7de52353577
Silvano Junior <silvano.marini.dev@gmail.com>
2025-12-12 17:55:59 -0300

    feat(aula191): add REQUESTS library for learning

---
+++ Curso_python3/aula191.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'http://localhost:8000/'
+response = requests.get(url)
+
+print(response.status_code)
+# print(response.headers)
+# print(response.content)
+# print(response.json())
+print(response.text)
